57b436a245189afb920811bc931ad0ff1cfd38ef
Eatowl <csnelcov@gmail.com>
2017-01-18 23:33:11 +0700

    sockaddr_un

--- client.c
+++ client.c
@@ -6,32 +6,34 @@
 #include <stdlib.h>
 #include <resolv.h>
 #include <string.h>
+#include <stdio.h>
+#include <sys/un.h>
 
 #define PORT_TIME 10
 #define MAX_BUF 128
-#define HOST "127.0.0.1"
+#define SOCK_NAME "mysocket1"
 
 int main() {
-	struct sockaddr_in dest;
-	char *host = "127.0.0.1";
 	char buffer[MAX_BUF];
-	int sd;
-	sd = socket(PF_INET, SOCK_STREAM, 0);
-	bzero(&dest, sizeof(dest));
-	dest.sin_family = AF_INET;
-	dest.sin_port = htons(PORT_TIME);
-	inet_aton(host, &dest.sin_addr);
-	if (connect(sd, (struct sockaddr_in *) &dest, sizeof(dest)) != 0) {
+	int cl_sock;
+	struct sockaddr_un saddr;
+	cl_sock = socket(PF_UNIX, SOCK_STREAM, 0);
+	bzero(&saddr, sizeof(saddr));
+	saddr.sun_family = AF_UNIX;
+	strcpy (saddr.sun_path, SOCK_NAME);
+	if (connect(cl_sock, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
 		perror("socket connection failed");
-		abort();
+		return 1;
 	}
-	fflush(stdin);
-	fgets(buffer, MAX_BUF, stdin);
-	//sprintf(buffer, "%s\n", MAX_BUF);
-	send(sd, buffer, strlen(buffer), 0);
-	bzero(buffer, MAX_BUF);
-	recv(sd, buffer, MAX_BUF - 1, 0);
-	printf("%s\n", buffer);
-	close(sd);
+	while (1) {
+		fflush(stdin);
+		fgets(buffer, MAX_BUF, stdin);
+		//sprintf(buffer, "%s\n", MAX_BUF);
+		write(cl_sock, buffer, strlen(buffer));
+		//if (!strcmp(buffer, "exit"))
+		//	break;
+		bzero(buffer, MAX_BUF);
+	}	
+	close(cl_sock);
 	return 0;
 }
--- server.c
+++ server.c
@@ -6,14 +6,42 @@
 #include <stdlib.h>
 #include <resolv.h>
 #include <string.h>
+#include <stdio.h>
+#include <sys/un.h>
 
+#define QUEUE_LENGTH 12
 #define MAX_BUF 128
+#define SOCK_NAME "mysocket1"
 
 int main() {
-	int sd;
+	int server_socket, client_socket;
+	struct sockaddr_un saddr;
 	char *buf;
-	sd = socket(PF_INET, SOCK_STREAM, 0);
+	server_socket = socket(PF_UNIX, SOCK_STREAM, 0);
 	buf = (char *) malloc(MAX_BUF);
-	
+	bzero(&saddr, sizeof(saddr));
+	saddr.sun_family = AF_UNIX;
+	strcpy (saddr.sun_path, SOCK_NAME);
+	if (bind(server_socket, (struct sockaddr *) &saddr, SUN_LEN(&saddr)) == -1) {
+		perror("bind() error");
+		return 1;
+	}
+	if (listen(server_socket, QUEUE_LENGTH) == -1) {
+		perror("listen() error");
+		return 0;
+	}
+	int count;
+	while(1) {
+		client_socket = accept(server_socket, NULL, NULL);
+		count = read(client_socket, buf, MAX_BUF - 1);
+		buf[count] = '\0';
+		printf(">> %s\n", buf);
+		close(client_socket);
+		if (!strcmp(buf, "exit"))
+			break;
+	}
+	free(buf);
+	close(server_socket);
+	unlink(SOCK_NAME);
 	return 0;
 }
