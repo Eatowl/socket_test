20ab00fbe3bfe393c02777dceadd92a3adc5a777
Eatowl <csnelcov@gmail.com>
2017-01-19 00:21:57 +0700

    update client-server

--- client.c
+++ client.c
@@ -10,28 +10,35 @@
 #include <sys/un.h>
 
 #define PORT_TIME 10
-#define MAX_BUF 128
+#define MAX_BUF 256
 #define SOCK_NAME "mysocket1"
 
 int main() {
 	char buffer[MAX_BUF];
+	char message[MAX_BUF];
 	int cl_sock;
-	struct sockaddr_un saddr;
-	cl_sock = socket(PF_UNIX, SOCK_STREAM, 0);
+	struct sockaddr_in saddr;
+	cl_sock = socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&saddr, sizeof(saddr));
-	saddr.sun_family = AF_UNIX;
-	strcpy (saddr.sun_path, SOCK_NAME);
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(3425);
+	saddr.sin_addr.s_addr=inet_addr("127.0.0.2");
 	if (connect(cl_sock, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
 		perror("socket connection failed");
 		return 1;
 	}
 	while (1) {
 		fflush(stdin);
-		fgets(buffer, MAX_BUF, stdin);
-		//sprintf(buffer, "%s\n", MAX_BUF);
-		write(cl_sock, buffer, strlen(buffer));
-		//if (!strcmp(buffer, "exit"))
-		//	break;
+		if(!strcmp(fgets(buffer, MAX_BUF, stdin), "exit")) {
+			close(cl_sock);
+			return 0;
+		}
+		printf("отправка сообщения на сервер...\n");
+    	send(cl_sock, buffer, sizeof(buffer), 0); // отправка сообщения на сервер
+    	int bytes_read = 0;
+    	printf("Ожидание сообщения\n");
+    	bytes_read = recv(cl_sock, message, sizeof(buffer), 0);
+    	printf("Получено %d bytes\tСообщение: %s\n", bytes_read, message); // прием сообщения от сервера
 		bzero(buffer, MAX_BUF);
 	}	
 	close(cl_sock);
--- server.c
+++ server.c
@@ -11,7 +11,6 @@
 
 #define QUEUE_LENGTH 12
 #define MAX_BUF 256
-#define SOCK_NAME "mysocket"
 
 int main() {
 	int server_socket, client_socket;
@@ -20,12 +19,10 @@ int main() {
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	buf = (char *) malloc(MAX_BUF);
 	bzero(&saddr, sizeof(saddr));
-	//saddr.sun_family = AF_INET;
 	saddr.sin_family = AF_INET;
 	saddr.sin_port = htons(3425);
-	saddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-	//strcpy (saddr.sun_path, SOCK_NAME);
-	if (bind(server_socket, (struct sockaddr *) &saddr, sizeof(&saddr)) == -1) {
+	saddr.sin_addr.s_addr=inet_addr("127.0.0.2");
+	if (bind(server_socket, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
 		perror("bind() error");
 		return 1;
 	}
@@ -36,14 +33,10 @@ int main() {
 	int count;
 	while(1) {
 		client_socket = accept(server_socket, NULL, NULL);
-
-		while(1)
-        {
+		while(1) {
             printf("Ожидаем сообщение...\n");
- 
- 
             count = recv(client_socket, buf, 256, 0); // принимаем сообщение от клиента
-            if(count <= 0) break;
+            if(!strcmp(buf, "exit")) break;
             printf("Получено %d bytes\tСообщение: %s\n", count, buf);
             printf("Отправляю принятое сообщение клиенту\n");
             send(client_socket, buf, count, 0); // отправляем принятое сообщение клиенту
@@ -51,16 +44,9 @@ int main() {
     
         close(client_socket); // закрываем сокет
         _exit(0);
-
-		/*count = read(client_socket, buf, MAX_BUF - 1);
-		buf[count] = '\0';
-		printf(">> %s\n", buf);
-		close(client_socket);
-		if (!strcmp(buf, "exit"))
-			break;*/
 	}
 	free(buf);
 	close(server_socket);
-	unlink(SOCK_NAME);
+	//unlink(SOCK_NAME);
 	return 0;
 }
